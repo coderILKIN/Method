3fd29f88ec0143428d7a06955e83673f780f4c1e
Ilkin Yusubov <tu758403c@code.edu.az>
2022-03-10 13:00:58 +0400

    . Ededlerden ibaret array verilib, bu ededlerin en boyuyunu tapmali.

---
+++ new task3/new task3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace new_task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            
+            
+        }
+
+
+
+          static void Max(string[] ary)
+        {
+            int[] arr = { 10, 4, 8, 20, 32, 2 };
+            int maxnumber = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]>maxnumber)
+                {
+                    maxnumber = arr[i];
+
+                }
+
+            }
+
+            Console.WriteLine(maxnumber);
+        }
+    }
+}
