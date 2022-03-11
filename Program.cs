6c0ba13dbb05f15e5647a67758d1bc9869bf7547
coderILKIN <99831207+coderILKIN@users.noreply.github.com>
2022-03-11 17:26:43 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace new_task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 10, 4, 8, 20, 32, 2 };
+
+            Console.WriteLine(Max(arr));
+        }
+
+
+
+        public static int Max(int [] arr)
+        {
+            
+            int maxnumber = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+                if (maxnumber < arr[i])
+                {
+                    maxnumber = arr[i];
+
+                }
+                
+            }
+            
+            return maxnumber;
+        }
+    }
+}
